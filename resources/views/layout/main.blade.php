809bcdfc109d8521b7ffac81f1aa0da45b8e6ebf
Jachym Pivonka <jachympivonka@gmail.com>
2022-10-31 22:30:27 +0100

    change css, head

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Festival Podzimní snění">
+    <meta property="og:image" content="/public/img/PS_2022_motivy-13.svg">
     <link rel="stylesheet" href="app.css">
     <link rel="icon" type="image/x-icon" href="img/PS_2022_motivy-02.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
